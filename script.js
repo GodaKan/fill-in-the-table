129ccfb10f6290967c436a257e268658177a7ac9
GodaKan <127443451+GodaKan@users.noreply.github.com>
2023-04-22 16:09:38 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+"use strict"
+
+// var input = document.getElementsByClassName("input")
+var fname = document.getElementById("fname")
+var surname = document.getElementById("surname")
+var age = document.getElementById("age")
+var add = document.getElementById("add")
+var table = document.getElementById("table")
+var removeFirst = document.getElementById("removeFirst")
+var removeLast = document.getElementById("removeLast")
+
+
+add.addEventListener("click", function(){
+
+    if(fname.value == "" || surname.value == "" || age.value == ""){
+        alert("Užpildykite visus laukus")
+    } 
+    else if (isNaN(age.value) || age.value < 1) {
+        alert("Amžiaus lauke nurodykite teigiamą skaičių");
+    }
+    else {
+        var row = table.insertRow(-1);
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        var cell3 = row.insertCell(2);
+        cell1.innerText = fname.value;
+        cell2.innerText = surname.value;
+        cell3.innerText = age.value;
+        fname.value = ""
+        surname.value = ""
+        age.value = ""
+    } 
+    })
+
+
+removeFirst.addEventListener("click", function(){
+    var i = table.rows.length
+    if(i > 1){
+        var row = table.deleteRow(1);
+    } else {
+        alert("Ištrynėte paskutinę eilutę")
+    }
+    
+})
+
+removeLast.addEventListener("click", function(){
+    var i = table.rows.length
+    if(i > 1){
+    var row = table.deleteRow(-1);
+    } else {
+    alert("Ištrynėte paskutinę eilutę") 
+    }
+})
